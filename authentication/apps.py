4e2009ff7c685b8ad6902cf1484961daf8042ff3
Kiidle <jonas.buechi@4teamwork.ch>
2023-09-09 00:50:10 +0200

    uncomment authentication handle

--- authentication/apps.py
+++ authentication/apps.py
@@ -6,11 +6,11 @@ class AuthenticationConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "authentication"
 
-    # def ready(self):
-    #     post_migrate.connect(self.run_after_migration, sender=self)
-    #
-    # def run_after_migration(self, sender, **kwargs):
-    #     from django.core.management import call_command
-    #     call_command('setup_roles')
-    #     call_command('setup_permissions')
-    #     call_command('init_officesync')
+    def ready(self):
+        post_migrate.connect(self.run_after_migration, sender=self)
+
+    def run_after_migration(self, sender, **kwargs):
+        from django.core.management import call_command
+        call_command('setup_roles')
+        call_command('setup_permissions')
+        call_command('init_officesync')

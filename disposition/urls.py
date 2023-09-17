4f870b4ba8705bbc0beb9c110ae4786a9f2c6b9c
Kiidle <jonas.buechi@4teamwork.ch>
2023-09-17 09:52:14 +0200

    Migration & Dispo

--- disposition/urls.py
+++ disposition/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from disposition.views import ToursView
+from disposition.views import ToursView, VehiclesView, CreateVehicleView
 
 urlpatterns = [
     path("", ToursView.as_view(), name="tours"),
+    path("vehicles/", VehiclesView.as_view(), name="vehicles"),
+    path("vehicles/create", CreateVehicleView.as_view(), name="vehicles_create")
 ]
--- disposition/views.py
+++ disposition/views.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse_lazy
 from django.views import generic
 
+from administration.models import Log
 from authentication.models import OfficeSync
-from disposition.models import Tour
+from disposition.models import Tour, Vehicle
+
 
 # Create your views here.
 
@@ -12,6 +15,71 @@ class ToursView(LoginRequiredMixin, generic.ListView):
     model = Tour
     fields = ["name"]
     template_name = "pages/tours/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["officesync"] = OfficeSync.objects.first()
+        return context
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            if not request.user.advanced.privacy:
+                return redirect("privacy")
+
+            if not request.user.advanced.terms:
+                return redirect("terms")
+
+            if not request.user.advanced.copyright:
+                return redirect("copyright")
+
+        return super().dispatch(request, *args, **kwargs)
+
+
+class VehiclesView(LoginRequiredMixin, generic.ListView):
+    model = Vehicle
+    fields = ["name"]
+    template_name = "pages/vehicles/index.html"
+    context_object_name = "vehicles"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["officesync"] = OfficeSync.objects.first()
+        return context
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            if not request.user.advanced.privacy:
+                return redirect("privacy")
+
+            if not request.user.advanced.terms:
+                return redirect("terms")
+
+            if not request.user.advanced.copyright:
+                return redirect("copyright")
+
+        return super().dispatch(request, *args, **kwargs)
+
+
+class CreateVehicleView(LoginRequiredMixin, generic.CreateView):
+    model = Vehicle
+    fields = ["license_plate", "name", "type", "model", "manufacturer", "year_of_manufacturer", "vin", "power_engine",
+              "capacity", "fuel_type", "fuel_consumption", "insurance", "condition"]
+    template_name = "pages/vehicles/form.html"
+
+    def get_success_url(self):
+        return reverse_lazy("vehicles")
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        Log.objects.create(
+            user=self.request.user,
+            action="CREATE",
+            content_object=self.object,
+            message=f"@{self.request.user} hat das Fahrzeug {self.object.license_plate} erstellt."
+        )
+
+        return response
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
